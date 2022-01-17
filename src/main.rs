7791e2a6ab17641c548dc56216760201f2d9422b
Aya Igarashi <aigarash@zlab.co.jp>
2022-01-17 19:46:42 +0900

    impl inc cmd

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use clap::{Parser, Subcommand};
+use reqwest::blocking::Client;
+use reqwest::header::CONTENT_LENGTH;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -32,14 +34,35 @@ fn main() {
     let path = pb.to_str().unwrap().to_string();
 
     let mut file = File::open(path).expect("configuration file not find");
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)
+    let mut token = String::new();
+    file.read_to_string(&mut token)
         .expect("fail to read configuration");
-    println!("content={}", contents);
+    token = token.trim().to_string();
 
+    let client = Client::new();
     match &args.command {
         Commands::Inc { graph } => {
-            todo!("increment {} pixel", graph);
+            let url = format!(
+                "https://pixe.la/v1/users/ladicle/graphs/{}/increment",
+                graph
+            )
+            .to_string();
+
+            let resp = match client
+                .put(url)
+                .header("X-USER-TOKEN", token)
+                .header(CONTENT_LENGTH, 0)
+                .send()
+            {
+                Ok(resp) => resp,
+                Err(err) => panic!("fail to send request: {}", err),
+            };
+
+            if resp.status().is_success() {
+                println!("{} is incremented!", graph);
+            } else {
+                panic!("{:?} Error", resp.status());
+            }
         }
         Commands::Get { resource } => {
             todo!("get {} resource", resource);
